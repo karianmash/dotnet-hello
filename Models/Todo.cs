2acf511ab70d87c8baab01720ab4806c98ddf044
karianmash <ianmachariak17@gmail.com>
2022-12-16 14:01:23 +0300

    initial commit

---
+++ Models/Todo.cs
@@ -0,0 +1,12 @@
+namespace Todo.Models;
+
+public class TodoModel
+{
+    // public string? RequestId { get; set; }
+
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    // public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
